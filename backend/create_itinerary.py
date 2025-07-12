97b36018ee7d6fd088495c50133017829eb3d1a8
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-07-12 15:16:21 -0400

    create command outputs json

--- backend/create_itinerary.py
+++ backend/create_itinerary.py
@@ -1,12 +1,13 @@
 from google import genai
 from google.genai import types
+import json
 
 from backend.itinerary_functions.temperature_function import get_temperature
 
 
 def create_itinerary(client: genai.Client, location: str) -> str:
 
-	config = types.GenerateContentConfig(tools=[get_temperature], system_instruction="Create a travel itinerary for a given location. Do not ask for more information. Keep your output under 900 characters. Do not use markup or special characters.",
+	config = types.GenerateContentConfig(tools=[get_temperature], system_instruction="Create a travel itinerary for {{location}}. Return it strictly as JSON in this shape:\n{\n  \"itinerary\": [\n    { \"day\": \"Day 1–3\", \"location\": \"Los Angeles, CA\", \"activities\": [\"Explore Hollywood\",\"Santa Monica Pier\",\"Griffith Observatory\",\"Universal Studios Hollywood or Disneyland\"] }\n  ]\n}\n Do not ask for more information. Keep your output under 900 characters. Do not use markup or special characters.",
 	                                     thinking_config=types.ThinkingConfig(
 	  thinking_budget=0)  # Disables thinking
 	  )
@@ -17,5 +18,6 @@ def create_itinerary(client: genai.Client, location: str) -> str:
 	    config=config
 	)	
 	itinerary = response.text
+	
 	assert itinerary
 	return itinerary	
