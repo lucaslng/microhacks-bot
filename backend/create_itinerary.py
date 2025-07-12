47c57c63876f8b79ed6116af06f1aab8c5e2a302
lucaslng <saculgnuel1@gmail.com>
2025-07-13 04:01:05 +0800

    its not using the functions

--- backend/create_itinerary.py
+++ backend/create_itinerary.py
@@ -4,6 +4,7 @@ import json
 
 from backend.geocoder import get_place_id
 from backend.itinerary_functions.temperature_function import get_temperature
+from backend.itinerary_functions.location_functions import get_restauraunts, get_place
 # import backend.itinerary_functions.directions_function
 
 
@@ -13,7 +14,7 @@ def create_itinerary(client: genai.Client, location: str) -> str:
 
 	contents = f"{location}\n{place_id}"
 
-	config = types.GenerateContentConfig(tools=[get_temperature], system_instruction="Create a travel itinerary for {{location}}. Return it strictly as JSON in this shape:\n{\n  \"itinerary\": [\n    { \"day\": \"Day 1–3\", \"location\": \"Los Angeles, CA\", \"activities\": [\"Explore Hollywood\",\"Santa Monica Pier\",\"Griffith Observatory\",\"Universal Studios Hollywood or Disneyland\"] }\n  ]\n}\n Do not ask for more information. You will be given the name of the location and then the google maps place_id of the location on seperate lines. Keep your output under 900 characters. Do not use markup or special characters.",
+	config = types.GenerateContentConfig(tools=[get_temperature, get_restauraunts, get_place], system_instruction="Create a travel itinerary for {{location}}. Return it strictly as JSON in this shape:\n{\n  \"itinerary\": [\n    { \"day\": \"Day 1–3\", \"location\": \"Los Angeles, CA\", \"activities\": [\"Explore Hollywood\",\"Santa Monica Pier\",\"Griffith Observatory\",\"Universal Studios Hollywood or Disneyland\"] }\n  ]\n}\n Do not ask for more information. You will be given the name of the location and then the google maps place_id of the location on seperate lines. Use the place_id with the functions to gain more information and find restauraunts along the route. Keep your output under 900 characters. Do not use markup or special characters.",
 	                                     thinking_config=types.ThinkingConfig(
 	  thinking_budget=0)  # Disables thinking
 	  )
---
+++ backend/itinerary_functions/location_functions.py
@@ -0,0 +1,74 @@
+import requests
+from util.apikeys import gmaps_key
+
+
+def get_restauraunts(place_id: str, radius: int) -> list:
+  """Gets the restauraunts within a given location.
+
+  Args:
+      place_id: The google maps place_id of the location to find restauraunts around, e.g. ChIJiQHsW0m3j4ARm69rRkrUF3w
+      radius: The radius to search for restauraunts around the area in meters, e.g. 200
+
+  Returns:
+      A list containing the google maps place_ids of all the restauraunts found.
+  """
+
+  headers = {
+    'X-Goog-Api-Key': gmaps_key,
+    'Content-Type': 'application/json',
+  }
+
+  json_data = {
+      "insights": [
+          "INSIGHT_PLACES",
+      ],
+      "filter": {
+          "locationFilter": {
+              "circle": {
+                  "place": f"places/{place_id}",
+                  "radius": radius
+              }
+          },
+          "typeFilter": {
+              "includedTypes": [
+                  "restaurant"
+              ]
+          },
+          "operatingStatus": [
+              "OPERATING_STATUS_OPERATIONAL",
+              "OPERATING_STATUS_TEMPORARILY_CLOSED"
+          ],
+          "ratingFilter": {
+              "minRating": 4.0,
+              "maxRating": 5
+          }
+      }
+  }
+
+  response = requests.post(
+    'https://areainsights.googleapis.com/v1:computeInsights', headers=headers, json=json_data)
+  data: dict[str, str] = response.json()["placeInsights"][0]
+  print("Called get_restauraunts function:", data)
+  return [v.removeprefix("places/") for _, v in data]
+
+
+def get_place(place_id: str) -> dict:
+  """Gets the information of a place using a google maps place_id.
+
+  Args:
+      place_id: The google maps place_id of the location to get information on, e.g. ChIJiQHsW0m3j4ARm69rRkrUF3w
+
+  Returns:
+      A dictionary containing the name, address of the location as well as information about things nearby. 
+  """
+
+  headers = {
+    'Content-Type': 'application/json',
+    'X-Goog-Api-Key': gmaps_key,
+    'X-Goog-FieldMask': 'displayName,formattedAddress,addressDescriptor',
+      }
+
+  response = requests.get(
+      f'https://places.googleapis.com/v1/places/{place_id}', headers=headers)
+  print("Called get_place function:", response.text)
+  return response.json()
