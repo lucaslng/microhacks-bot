e8f3533b4c52555a54ca5141700773d5446efbd5
lucaslng <saculgnuel1@gmail.com>
2025-07-13 03:19:28 +0800

    merge

--- backend/create_itinerary.py
+++ backend/create_itinerary.py
@@ -13,7 +13,7 @@ def create_itinerary(client: genai.Client, location: str) -> str:
 
 	contents = f"{location}\n{place_id}"
 
-	config = types.GenerateContentConfig(tools=[get_temperature], system_instruction="Create a travel itinerary for {{location}}. Return it strictly as JSON in this shape:\n{\n  \"itinerary\": [\n    { \"day\": \"Day 1–3\", \"location\": \"Los Angeles, CA\", \"activities\": [\"Explore Hollywood\",\"Santa Monica Pier\",\"Griffith Observatory\",\"Universal Studios Hollywood or Disneyland\"] }\n  ]\n}\n Do not ask for more information. Keep your output under 900 characters. Do not use markup or special characters.",
+	config = types.GenerateContentConfig(tools=[get_temperature], system_instruction="Create a travel itinerary for {{location}}. Return it strictly as JSON in this shape:\n{\n  \"itinerary\": [\n    { \"day\": \"Day 1–3\", \"location\": \"Los Angeles, CA\", \"activities\": [\"Explore Hollywood\",\"Santa Monica Pier\",\"Griffith Observatory\",\"Universal Studios Hollywood or Disneyland\"] }\n  ]\n}\n Do not ask for more information. You will be given the name of the location and then the google maps place_id of the location on seperate lines. Keep your output under 900 characters. Do not use markup or special characters.",
 	                                     thinking_config=types.ThinkingConfig(
 	  thinking_budget=0)  # Disables thinking
 	  )
